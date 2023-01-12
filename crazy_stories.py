46e2773a5b725ee718faee57ae79d111e8785a6c
Oliver31Batista <ohenriquez@intellisys.com.do>
2023-01-12 11:52:00 -0400

    mad libs finished

---
+++ crazy_stories.py
@@ -0,0 +1,16 @@
+# organization =  "freeCodeCamp"
+
+# print("Learn to programming with " + organization)
+# print("Learn to programming with {}".format(organization))
+# print(f"Learn to programming with {organization}")# f- string
+             
+# Mad Libs
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+plural_substantive = input("Plural substantive: ")
+
+madlib = f"¡Programming is so {adj}! It has always excited me because I love {verb1} problems. ¡Learn to {verb2} width freeCodeCamp and reach your {plural_substantive}!"
+
+print(madlib)
